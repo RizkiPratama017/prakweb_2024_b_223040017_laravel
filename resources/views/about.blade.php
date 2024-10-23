f9ccad347529d7f9916baae63dc92534fb15ef1e
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2024-10-23 12:05:17 +0700

    laravel 11 - video struktur folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman About</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+    <h3>Owner : {{ $nama }}</h3>
+
+    <h1>halaman About</h1>
+
+    <img src="img/profil.jpg" alt="">
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman home</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+
+    <h1>halaman home</h1>
+    
+
+
+    <script src="js/script.js"></script>
+</body>
+</html>
