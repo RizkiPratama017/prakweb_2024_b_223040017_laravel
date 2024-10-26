cc7cb142b70b17ea683e4fd2caa509dba847f95e
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2024-10-26 19:23:57 +0700

    laravel 11 - video view data

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,5 @@
+@props(['active' => false])
+
 <a {{ $attributes }} href="/"
     class=" {{ $active ? ' bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}"
     aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,62 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Rizki Pratama'], ['title' => 'About']);
+    return view('about', ['nama' => 'Rizki Pratama', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => 'Blog',
+        'posts' => [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Rizki Pratama',
+                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati consequuntur eligendi quia adipisci neque fuga necessitatibus quisquam maiores cum iusto! Debitis et alias neque earum vero odit ratione eum id?'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Rizki Pratama',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae tempore facere aut modi rerum, quis est commodi minima vitae nisi ipsa a accusantium dolores aliquid ex pariatur perspiciatis harum.'
+            ]
+        ]
+    ]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Rizki Pratama',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati consequuntur eligendi quia adipisci neque fuga necessitatibus quisquam maiores cum iusto! Debitis et alias neque earum vero odit ratione eum id?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rizki Pratama',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae tempore facere aut modi rerum, quis est commodi minima vitae nisi ipsa a accusantium dolores aliquid ex pariatur perspiciatis harum.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
