1c07ff3ae6a049d4587dbbd005b49c7505559abe
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2024-10-31 21:34:29 +0700

    laravel 11 - video searching

--- routes/web.php
+++ routes/web.php
@@ -3,46 +3,35 @@
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
-    return view('home', ['title' => 'Home Page']);
+    return view('home', ['title' => 'Halaman Utama']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Rizki Pratama', 'title' => 'About']);
+    return view('about', ['nama' => 'Rizki Pratama', 'title' => 'Tentang Kami']);
 });
 
 Route::get('/posts', function () {
-
-    // $posts = Post::with(['author', 'category'])->latest()->get();
-    $posts = Post::latest()->get();
-
     return view('posts', [
         'title' => 'Blog',
-        'posts' => $posts
+        'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()
     ]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
-
-    return view('post', ['title' => 'Single Post', 'post' => $post]);
+    return view('post', ['title' => 'Artikel', 'post' => $post]);
 });
+
 Route::get('/authors/{user:username}', function (User $user) {
-    // $posts = $user->posts->load('category', 'author');
-
-    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Artikel oleh ' . $user->name, 'posts' => $user->posts]);
 });
-Route::get('/categories/{category:slug}', function (Category $category) {
-    // $posts = $category->posts->load('category', 'author');
 
-    return view('posts', ['title' => 'Articles in: '  .   $category->name, 'posts' => $category->posts]);
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', ['title' => 'Artikel dalam Kategori: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact']);
+    return view('contact', ['title' => 'Kontak']);
 });
